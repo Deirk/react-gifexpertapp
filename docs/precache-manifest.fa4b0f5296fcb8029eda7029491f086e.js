e8c5290d284f7419fef2ab2e638cc811841b6561
deirk <deirkh@gmail.com>
2021-01-31 22:41:57 -0500

    Docs add

---
+++ docs/precache-manifest.fa4b0f5296fcb8029eda7029491f086e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "354d38e79f9bdf6ad2aa07dede5e5eb8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f10f84b20867bc6b7a23",
+    "url": "/static/css/main.ba0575d8.chunk.css"
+  },
+  {
+    "revision": "18772ccb525ea2d14384",
+    "url": "/static/js/2.57075067.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.57075067.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f10f84b20867bc6b7a23",
+    "url": "/static/js/main.29b7ccd7.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
